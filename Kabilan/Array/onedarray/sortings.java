69c02e1b44fb8468ec73abe6a0d75e0b630ab95f
kabilankavi131 <kabilankavi131@gmail.com>
2024-02-10 22:12:04 +0530

    Sortings Algorithm Added

--- Kabilan/Array/onedarray/sortings.java
+++ Kabilan/Array/onedarray/sortings.java
@@ -1,8 +1,7 @@
 package Kabilan.Array.onedarray;
 
 public class sortings {
-
-    static int[] bubblesort(int[] marks, int len) {
+    static int[] bubbleSort(int[] marks, int len) {
         int flag = 1;
         for (int i = 0; i < len - 1; i++) {
             for (int j = 0; j < len - 1 - i; j++) {
@@ -19,18 +18,77 @@ public class sortings {
         return marks;
     }
 
+    static int[] selectionSort(int[] marks, int len) {
+        for (int i = 0; i < len - 1; i++) {
+            int min = i; // Assume the first value as minimum value
+            for (int j = i + 1; j < len; j++) {
+                if (marks[j] < marks[min])
+                    min = j;
+            }
+            if (min != i) { // To check whether the minimum value is not the initial value which we
+                            // assumed,if it is then we don't have to swap them anymore bcoz it just take
+                            // some extra time!!
+                int temp = marks[i];
+                marks[i] = marks[min];
+                marks[min] = temp;
+            }
+        }
+        return marks;
+    }
+
+    static int[] insertionSort(int[] marks, int len) {
+        for (int i = 1; i < len; i++) {
+            int temp = marks[i];
+            int j = i - 1;
+            while (j >= 0 && marks[j] > temp) {
+                marks[j + 1] = marks[j]; // here the value is actually over writted!! so don't get confused
+                j--;
+            }
+            marks[j + 1] = temp;
+        }
+        return marks;
+    }
+
     public static void main(String[] args) {
         int marks[] = { 100, 60, 90, 70, 80 };
         int len = marks.length;
+        System.out.println("Bubble Sorting..");
         System.out.println("Before Sorting..");
         for (int i : marks)
             System.out.print(i + " ");
         System.out.println();
-        marks = bubblesort(marks, len);
+        marks = bubbleSort(marks, len);
         System.out.println("After Sorting..");
         for (int i : marks) {
             System.out.print(i + " ");
         }
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Insertion Sorting..");
+        System.out.println("Before Sorting..");
+        for (int i : marks)
+            System.out.print(i + " ");
+        System.out.println();
+        marks = insertionSort(marks, len);
+        System.out.println("After Sorting..");
+        for (int i : marks) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Selection Sorting..");
+        System.out.println("Before Sorting..");
+        for (int i : marks)
+            System.out.print(i + " ");
+        System.out.println();
+        marks = selectionSort(marks, len);
+        System.out.println("After Sorting..");
+        for (int i : marks) {
+            System.out.print(i + " ");
+        }
+
     }
 }
 /*
@@ -38,4 +96,22 @@ public class sortings {
  * 100 60 90 70 80
  * After Sorting..
  * 60 70 80 90 100
+ */
+
+/*
+ * Time Complexity :
+ * Bubble Sort:
+ * 
+ * Best Case: O(n) - When the array is already sorted.
+ * Worst Case: O(n^2) - When the array is in reverse order.
+ * 
+ * Selection Sort:
+ * 
+ * Best Case: O(n^2) - Same as the worst case.
+ * Worst Case: O(n^2) - No matter the initial order of the array.
+ * 
+ * Insertion Sort:
+ * 
+ * Best Case: O(n) - When the array is almost sorted.
+ * Worst Case: O(n^2) - When the array is in reverse order.
  */
